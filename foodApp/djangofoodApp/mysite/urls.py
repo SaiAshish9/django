e944c7a4a7425451665717bac06634397458471b
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2020-05-10 16:31:44 +0530

    used pillow

--- foodApp/djangofoodApp/mysite/urls.py
+++ foodApp/djangofoodApp/mysite/urls.py
@@ -24,9 +24,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 from users import views as user_views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/',include('food.urls')),
     path('register/',user_views.register,name="register"),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name="login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
+    path('profile/',user_views.profilepage,name="profile"),
 ]
+
+
+urlpatterns+=[
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
